c60385f27b2ec62dfa3452bd0c6ddc8848fa0e55
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-08-05 10:04:51 -0500

    add ssr mode config

--- deal-online-frontend/astro.config.mjs
+++ deal-online-frontend/astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server", // Renderizado del lado del servidor
+  adapter: node({
+    mode: "standalone", // Para Cloud Run
+  }),
   // Removed vite.define - let Astro handle environment variables automatically
 });
